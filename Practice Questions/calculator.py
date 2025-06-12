3cf671e110ac912a6a22fcf6bdb33c2761742120
Mihika Sharma <163665705+MihikaS12@users.noreply.github.com>
2025-06-12 21:50:18 +0530

    Update calculator.py

--- Practice Questions/calculator.py
+++ Practice Questions/calculator.py
@@ -1,1 +1,21 @@
+num1= int(input("Enter 1st Number:"))
+num2= int(input("Enter 2nd Number:"))
+add= num1+num2
+diff=num1-num2
+prod=num1*num2
+div= num1/num2
+user=input("Enter the Operation You Want")
+if user=="sum":
+  print("The Sum is : ",sum)
+elif user=="difference":
+  print("The Difference is: ",diff)
+elif user=="product":
+  print("The Product is: ",prod)
+elif user=="division":
+  print("The Division is: ",div)
+
+else:
+  print("invalid option!")
+
+
 
